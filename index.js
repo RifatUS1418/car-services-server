7064a43a14859ff4b4038622802f37672c9aa655
unknown <rifatullah1418@gmail.com>
2021-12-18 20:12:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+import { MongoClient } from "mongodb";
+const port = process.env.PORT || 5000;
+
+
+
+
+// middleware
+app.use(cors());
+app / use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lgbbe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('car_hospital');
+        const serviceCollection = database.collection('services');
+
+        // GET services API
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello Car Hospital')
+})
+
+app.listen(port, () => {
+    console.log(`Listening at ${port}`)
+})
+
+
