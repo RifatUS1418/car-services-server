2ae112a4650edc4d5bbaef7a9bd9380c025b4347
unknown <rifatullah1418@gmail.com>
2021-12-19 17:28:59 +0600

    2nd commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-import { MongoClient } from "mongodb";
+const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-app / use(express.json());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lgbbe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
